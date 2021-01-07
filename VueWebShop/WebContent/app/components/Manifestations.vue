054f3af87ae72e0d5740e456d2f6edc91b31019c
Jelena Stojanovic <stojanovic.sw79.2018@uns.ac.rs>
2021-01-08 00:25:44 +0100

    Uradjene manifestacije pojedinacno-1. deo

--- VueWebShop/WebContent/app/components/Manifestations.vue
+++ VueWebShop/WebContent/app/components/Manifestations.vue
@@ -1,18 +1,23 @@
 <template>
-	<div class="row">
-		<div class="manifestation" v-bind:key="m.id" v-for="m in manifestations">
-			<div class="col-lg-3 col-md-4 col-sm-6">
-				
-				<!--<img v-bind:src = "contextPath + 'WebContent\\images\\' + m.image">-->
-				<!-- <img v-bind:src = "'..\\..\\images\\' + m.image"> -->
-				<img v-bind:src="'../../images/' + m.image">
-				<h2>{{m.name}}</h2>
-				<span>{{m.date.dayOfMonth}}</span>
-				<span>{{m.date.month}}</span>
-				<span>{{m.date.year}}</span>
-				<span>{{m.location.street}}</span>
-				<span>{{m.location.city}}</span>
-			</div>
+	<div id="manifestations" class="row" data-masonry='{"percentPosition": true }'>
+		<div class="col-lg-4 col-md-4 col-sm-6" v-bind:key="m.id" v-for="m in manifestations">
+				<div class="manifestation">
+					<div class="image-holder">
+						<img class="fit-img" v-bind:src="'images/' + m.image">
+					</div>
+					<div class="text-center w-100 pt-1 pb-1 manifestation-type">{{m.type}}</div>
+					<div class="description-holder">
+						
+                        <h3 class="name text-primary">{{m.name}}</h3>
+						<span>{{m.date.dayOfMonth}}</span>
+						<span>{{m.formattedMonth}}</span>
+						<span>{{m.date.year}}</span><br>
+						<span>{{m.location.street}}, </span>
+						<span>{{m.location.city}}</span>
+					</div>
+					
+				</div>
+			
 		</div>
 	</div>
 </template>
@@ -21,27 +26,122 @@
 module.exports = {
 	data() {
 		return {
-			manifestations: [],
-			contextPath: ""
+			manifestations: []
 		}
 	},
 	methods: {
-
+		makeDate : (manifestation) => {
+			
+			switch(manifestation.date.month) {
+				
+				case "JANUARY":
+					console.log("tu");
+					manifestation.formattedMonth = "januar";
+					break;
+				case "FEBRUARY":
+					manifestation.formattedMonth = "februar";
+					break;
+				case "MARCH":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "APRIL":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "MAY":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "JUNE":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "JULY":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "AUGUST":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "SEPTEMBER":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "OCTOBER":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "NOVEMBER":
+					manifestation.formattedMonth = "mart";
+					break;
+				case "DECEMBER":
+					manifestation.formattedMonth = "mart";
+					break;
+				default:
+					console.log("nista");
+			}
+		}
 	},
 	mounted() {
 		axios
 			.get("rest/manifestationservice/getall")
 			.then(response => {
+				
 				this.manifestations = response.data;
-			});
-		axios
-			.get("rest/manifestationservice/getcontextpath")
-			.then(response => {
-				this.contextPath = response.data;
+				console.log(this.manifestations);
+				this.manifestations.forEach(manifestation => this.makeDate(manifestation));
 			});
 	}
 }
 </script>
 
 <style>
+
+#manifestations {
+	margin-top:4em;
+}
+
+.manifestation {
+	border:1px solid darkgray;
+	margin-bottom: 20px;
+}
+
+/* .image-holder img {
+	max-width: 100%;
+}
+
+.image-holder {
+	overflow: hidden;
+}
+
+@media screen and (max-width: 1200px) {
+  .image-holder {
+	min-height: 10em;
+	max-height: 10em;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+	.image-holder {
+		height: 20em;
+	}
+}
+
+@media screen and (max-width: 575px) {
+	.image-holder {
+		min-height: 30em;
+	    max-height: 30em;
+	}
+} */
+
+.fit-img {
+    width: 100%;
+}
+
+.manifestation .name {
+	font-size: 20px;
+}
+
+.manifestation .description-holder {
+	padding:20px;
+}
+
+.manifestation .manifestation-type {
+    background-color: #bbbb;
+}
+
 </style>
